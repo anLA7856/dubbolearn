525faf7f707c7ea7537afcd9348c9c3367ef02cf
anLA7856 <luoan007@gmail.com>
2019-07-18 22:59:18 +0800

    增加 Callback 例子

---
+++ callback/callbackprovider/src/main/java/com/anla/rpc/callback/provider/service/CallbackListener.java
@@ -0,0 +1,14 @@
+package com.anla.rpc.callback.provider.service;
+
+/**
+ * @author anLA7856
+ * @date 19-7-18 下午10:09
+ * @description
+ */
+public interface CallbackListener {
+    /**
+     * 理解为方法，就是一个方法
+     * @param msg
+     */
+    void changed(String msg);
+}
---
+++ callback/callbackprovider/src/main/java/com/anla/rpc/callback/provider/service/CallbackService.java
@@ -0,0 +1,17 @@
+package com.anla.rpc.callback.provider.service;
+
+/**
+ * @author anLA7856
+ * @date 19-7-18 下午10:10
+ * @description
+ */
+public interface CallbackService {
+
+    /**
+     * 这个 索引为1的是callback类型。
+     * dubbo 将基于长连接生成反向代理，就可以在服务端调用客户端逻辑
+     * @param key
+     * @param listener
+     */
+    void addListener(String key, CallbackListener listener);
+}
