02265fe0e19ebb64678917017832c747cf21bb02
anLA7856 <luoan007@gmail.com>
2019-07-31 23:26:44 +0800

    增加 部分spi compatible 的例子

--- spi/spiprovider/src/main/java/com/anla/rpc/spi/provider/Provider.java
+++ spi/spiprovider/src/main/java/com/anla/rpc/spi/provider/Provider.java
@@ -13,7 +13,7 @@ public class Provider {
 
     public static void main(String[] args) throws Exception {
 
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/dubbo-demo-provider.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo/provider.xml");
         context.start();
 
         System.out.println("dubbo service started");
