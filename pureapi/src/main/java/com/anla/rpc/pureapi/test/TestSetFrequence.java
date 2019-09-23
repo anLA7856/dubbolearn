ff162dc7816ecfeb1a55d4215dfcc8acb5ee9242
luoan <luoan007@gmail.com>
2019-09-23 16:30:40 +0800

    增加 merger 例子

---
+++ pureapi/src/main/java/com/anla/rpc/pureapi/test/TestSetFrequence.java
@@ -0,0 +1,22 @@
+package com.anla.rpc.pureapi.test;
+
+import org.apache.dubbo.common.utils.ConcurrentHashSet;
+
+import java.util.Set;
+
+/**
+ * @author luoan
+ * @version 1.0
+ * @date 2019/8/28 9:58
+ **/
+public class TestSetFrequence {
+    public static void main(String[] args) {
+        Set<String> names = new ConcurrentHashSet<>();
+        names.add("dog");
+        names.add("cat");
+        names.add("mouse");
+        for (String s : names){
+            System.out.println(s);
+        }
+    }
+}
